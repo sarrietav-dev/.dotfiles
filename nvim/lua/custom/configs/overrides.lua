4fbe8bfbffcc648acb3c8de24161f39789ee675a
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2024-01-01 17:18:22 -0500

    Push custom config

---
+++ nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,72 @@
+local M = {}
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "go",
+    "rust",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "prettierd",
+    "tailwindcss-language-server",
+    "eslint-lsp",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+M.copilot = {
+  suggestion = {
+    enable = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+    },
+  },
+  panel = {
+    enable = false,
+  }
+}
+
+return M
