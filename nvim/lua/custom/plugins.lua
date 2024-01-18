d329833ee19ffecea537084dcabb16c178467171
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2024-01-17 20:48:08 -0500

    add rust plugin

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -73,6 +73,13 @@ local plugins = {
     cmd = { "Copilot" },
     opts = overrides.copilot,
   },
+  {
+    "rust-lang/rust.vim",
+    ft = { "rust" },
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  }
 
   -- To make a plugin not be loaded
   -- {
