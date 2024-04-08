8ebda26ca1d4e1667ddda0391a79188663335bbe
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-04-07 19:40:58 -0500

    Add nvim

---
+++ nvim/lua/configs/cmp.lua
@@ -0,0 +1,35 @@
+local cmp_ui = require("nvconfig").ui.cmp
+local cmp_style = cmp_ui.style
+
+local field_arrangement = {
+  atom = { "kind", "abbr", "menu" },
+  atom_colored = { "kind", "abbr", "menu" },
+}
+
+local formatting_style = {
+  -- default fields order i.e completion word + item.kind + item.kind icons
+  fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
+
+  format = function(entry, item)
+    local icons = require "nvchad.icons.lspkind"
+    local icon = (cmp_ui.icons and icons[item.kind]) or ""
+
+    if cmp_style == "atom" or cmp_style == "atom_colored" then
+      icon = " " .. icon .. " "
+      item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
+      item.kind = icon
+    else
+      icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
+      item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
+    end
+
+    return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+  end,
+}
+
+local opts = function()
+  local nvchad_ops = require "nvchad.configs.cmp"
+  nvchad_ops.formatting = formatting_style
+end
+
+return opts
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "gopls",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "go",
+        "typescript",
+        "javascript",
+        "tsx",
+        "rust",
+      },
+      autotag = {
+        enable = true,
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = require "configs.cmp",
+    config = function(_, opts)
+      require("cmp").setup(opts)
+    end,
+  },
+  { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = function()
+      vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline = true,
+        virtual_text = {
+          spacing = 5,
+          severity_limit = "Warning",
+        },
+        update_in_insert = true,
+      })
+    end,
+  },
+}
