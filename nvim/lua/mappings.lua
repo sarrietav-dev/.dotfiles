33e23f9fbcf9367f03d6f27b9261e5ced5d0c364
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-04-08 09:42:40 -0500

    add mapping for search highlighting

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,5 +5,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>nh", ":noh<cr>", { desc = "Clear last search highlighting" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
