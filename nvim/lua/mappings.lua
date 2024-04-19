98183767116ac765bfcb282444e12b910ac75fe8
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-04-19 16:05:22 -0500

    feat: add resizing mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>nh", ":noh<cr>", { desc = "Clear last search highlighting" })
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+map({ "n", "i", "v" }, "<A-Down>", "<C-w>5-", { desc = "Resize window height" })
+map({ "n", "i", "v" }, "<A-Up>", "<C-w>5+", { desc = "Resize window height" })
+map({ "n", "i", "v" }, "<A-Left>", "<C-w>5<", { desc = "Resize window width" })
+map({ "n", "i", "v" }, "<A-Right>", "<C-w>5>", { desc = "Resize window width" })
