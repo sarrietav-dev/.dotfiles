610a8be61cc6ad4bd3b08b187fca7ef91f278cc3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2025-12-27 16:32:26 -0500

    add blb

---
+++ install/install_blb.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+omarchy-webapp-remove "Blue Letter Bible"
+omarchy-webapp-install "Blue Letter Bible" https://blueletterbible.org https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/blue-letter-bible.png
